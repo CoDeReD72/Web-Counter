ad2dca8ad5458b35e8454d0248a9e6fa76097c31
Declan Gallagher <47104283+CoDeReD72@users.noreply.github.com>
2021-12-12 22:39:36 +0000

    Error Correction

--- app.js
+++ app.js
@@ -1,34 +1,34 @@
-// Initial Value Set to 0
+
 let count = 0;
 
-const value = document.querySelector("#value")
-const btns = document.querySelectorAll(".btn")
+
+//==== SELECTING HTML ELEMENTS ====
+const value = document.querySelector("#value");
+const btns = document.querySelectorAll(".btn");
 
 btns.forEach(function (btn) {
-    btn.addEventListener("click", function (e) {
+  btn.addEventListener("click", function (e) {
+    
+    //==== CHANGING COUNTER VALUE ====
+    const styles = e.currentTarget.classList;
+    if (styles.contains("decrease")) {
+      count--;
+    } else if (styles.contains("increase")) {
+      count++;
+    } else {
+      count = 0;
+    }
 
-        //==== CHANGING COUNTER ====
-        const styles = e.currentTarget.classList;
-        if (styles.contains("decrease")) {
-            count--;
-        }
-        else if (styles.contain("increase")) {
-            count++
-        }
-        else {
-            count = 0;
-        }
-
-        // ==== COLOUR GRADING ====
-        if (count > 0) {
-            value.style.color = "green";
-        }
-        if (count < 0) {
-            value.style.colour = "red";
-        }
-        if (count === 0) {
-            value.style.color = "#222";
-        }
-        value.textContent.Content = count;
-    });
+    //==== CHANGING COLOUR ====
+    if (count > 0) {
+      value.style.color = "green";
+    }
+    if (count < 0) {
+      value.style.color = "red";
+    }
+    if (count === 0) {
+      value.style.color = "#222";
+    }
+    value.textContent = count;
+  });
 });
